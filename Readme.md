e62835c6a4b1c27dd907d994258dc76faa7e6dff
Dmytro Komyshnyi <Dmytro_Komyshnyi@epam.com>
2022-07-13 19:40:34 +0300

    Update Readme.md

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 ##table input format:
-row1cell1,row1cell2,row1cell3
+row1cell1,row1cell2,row1cell3<br/>
 row2cell1,row2cell2,row2cell3
 ##table output
 ```
--- src/main/java/com/dmytro/komyshnyi/table/Main.java
+++ src/main/java/com/dmytro/komyshnyi/table/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         TableBuilder tableBuilder = new TableBuilder();
-        String table = "";
+        String table = "row1cell1,row1cell2,row1cell3\n" +
+                "row2cell1,row2cell2,row2cell3";
 
-        System.out.println(tableBuilder.build(""));
+        System.out.println(tableBuilder.build(table));
     }
 
 }
